be3ac183007e246192ae5600b9212616b02fa6d9
root <root@kali>
2020-07-03 14:31:25 -0400

    getting an immutable vector

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate ncurses;
 
 use ncurses::*;
 
-static WINDOW_HEIGHT: i32 = 3;
-static WINDOW_WIDTH: i32 = 10;
+// static WINDOW_HEIGHT: i32 = 3;
+// static WINDOW_WIDTH: i32 = 10;
 
 /**
  *  1. Any live cell with two or three live neighbours survives.
@@ -13,75 +13,37 @@ static WINDOW_WIDTH: i32 = 10;
 
 fn main() {
 
+    /* Create the screen */
     initscr();
     raw();
+    /* invisible cursor */
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
+    /* initialize more keys */
+    keypad(stdscr(), true);
+    noecho();
+
+    /* Get the screen bounds */
     let mut max_x = 0;
     let mut max_y = 0;
     getmaxyx(stdscr(), &mut max_y, &mut max_x);
 
-    keypad(stdscr(), true);
-    noecho();
-
-    /* Invisible cursor. */
-    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
-
-    /* Status/help info. */
-    addstr("To start press s, to quit at any time press q");
-    // mvprintw(LINES() - 1, 0, "Press F1 to exit"); bottom of the screen
+    /* Print a char at the max */
+    mv(max_y - 1,max_x - 1);
+    addstr("~");
     refresh();
 
-    let mut start_y = (max_y - WINDOW_HEIGHT) / 2;
-    let mut start_x = (max_x - WINDOW_WIDTH) / 2;
-    let mut win = create_win(start_y, start_x);
 
-    let mut ch = getch();
-    while ch != KEY_F(1) {
-        match ch {
-            KEY_LEFT => {
-                start_x -= 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            }
-            KEY_RIGHT => {
-                start_x += 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            },
-            KEY_UP => {
-                start_y -= 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            },
-            KEY_DOWN => {
-                start_y += 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            },
-            _ => { }
-        }
-
-        ch = getch();
-    }
+    /* Wait for input before killing the screen */
+    getch();
 
     endwin();
-}
+    let mut vec = vec![vec!['#'; max_x as usize]; max_y as usize];
+    for y in 1 ..max_y {
+        for x in 1 ..max_x {
+            print!("{}", vec[y as usize][x as usize]);
+        }
+        println!("");
+    }
 
-fn create_win(start_y: i32, start_x: i32) -> WINDOW {
-    mvprintw(start_y, start_x, "*");
-
-    let win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, start_y, start_x);
-    box_(win, 0, 0);
-    wrefresh(win);
-    win
-}
-
-fn destroy_win(win: WINDOW) {
-    let ch = ' ' as chtype;
-
-    // clear the previous screen
-    wborder(win, ch, ch, ch, ch, ch, ch, ch, ch);
-    
-    wrefresh(win);
-    delwin(win);
 }
